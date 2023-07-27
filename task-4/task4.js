edd981f599e43b3a20c96fa3f33b1520c489701c
bernadette <bernadette12ezghen@gmail.com>
2023-07-27 01:00:28 -0700

    done with task-7

---
+++ task-4/task4.js
@@ -0,0 +1,5 @@
+const myArray = [1,2,3,4,5,6,];
+const var1 = my name
+const var2 = my family name 
+const var3 =my number 
+const myArray = Array.of(var1 ,var2, var3);
---
+++ task-7/task7.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 20; i++) 
+{
+    if (i % 3 == 0) {
+      console.log('div3');
+    } else {
+      console.log(i);
+    }
+    if (i % 5 ==0){console.log('div5')}
+    else{console.log('div5')}
+    if (i% 3&5 ==0){
+    console.log('div3&5')}
+    else{console.log(i)}
+
+  }
+
+   
